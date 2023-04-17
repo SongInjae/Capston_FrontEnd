0c61c4a2c65eb638d698986f48041ab09ea590c7
kwonhyeongseok <rnjsgudtjr@naver.com>
2023-04-17 19:46:40 +0900

    Add user routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
+import SignIn from './User/client_container/signIn';
 import MainPage from './Manager/pages/MainPage';
-
+import UserMain from './User/client_container/main';
 const App = () => {
   return (
     <Routes>
-      <Route path="/*" element={<MainPage />} />
+      <Route path="/" element={<SignIn />} />
+      <Route path="/admin" element={<MainPage />} />
+      <Route path="/main" element={<UserMain />} />
     </Routes>
   );
 };
