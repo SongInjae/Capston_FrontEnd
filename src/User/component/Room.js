374bb2ad886ad178b201d7454c81258bcde6559a
kwonhyeongseok <rnjsgudtjr@naver.com>
2023-05-07 12:48:04 +0900

    add room component

--- src/User/component/Room.js
+++ src/User/component/Room.js
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 const Room = styled.div`
-    position: relative;
     border: solid;
     border-width: 2px;
     border-color: lightgray;
