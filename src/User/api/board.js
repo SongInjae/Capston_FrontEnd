1e087ec6d03b187cb8220095b10cbbd953d3def6
zoe <ool2o8@github.com>
2023-06-06 15:23:42 +0900

    .

--- src/User/api/board.js
+++
@@ -1,9 +0,0 @@
-import { board } from './client';
-import cookie from 'react-cookies';
-
-const token = cookie.load('token');
-
-export const takeAllInfo = () =>
-  board.get('http://localhost/api/utils/notice', {
-    headers: { Authorization: `Token ${token}` },
-  });
