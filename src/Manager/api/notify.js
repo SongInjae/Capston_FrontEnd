71322cab9e97f5668cb6905a6b04f66cb3eddec9
SongInJae <pan0663@naver.com>
2023-06-08 22:47:49 +0900

    fit_manegerAPI_CooikeLoad

--- src/Manager/api/notify.js
+++ src/Manager/api/notify.js
@@ -1,21 +1,19 @@
 import { notify } from '../../User/api/client';
 import cookie from 'react-cookies';
 
-const token = cookie.load('token');
-
 export const takeAllInfo = () =>
   notify.get('utils/notice', {
-    headers: { Authorization: `Token ${token}` },
+    headers: { Authorization: `Token ${cookie.load('token')}` },
   });
 export const addInfo = (formData) =>
   notify.post('utils/notice', formData, {
-    headers: { Authorization: `Token ${token}` },
+    headers: { Authorization: `Token ${cookie.load('token')}` },
   });
 export const removeInfo = (id) =>
   notify.delete(`utils/notice/${id}`, {
-    headers: { Authorization: `Token ${token}` },
+    headers: { Authorization: `Token ${cookie.load('token')}` },
   });
 export const changeInfo = ({ id, formData }) =>
   notify.put(`utils/notice/${id}`, formData, {
-    headers: { Authorization: `Token ${token}` },
+    headers: { Authorization: `Token ${cookie.load('token')}` },
   });
